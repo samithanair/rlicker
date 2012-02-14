a23da0e355646a010db70ad2fc799315ede2ce5a
Ben Biddington <ben.biddington@gmail.com>
2012-02-14 21:44:09 +1300

    RLicker can locate an element by id

--- spec/integration/capybara_webkit_spec.rb
+++ spec/integration/capybara_webkit_spec.rb
@@ -15,13 +15,27 @@ module RLicker
       
       def url; @browser.url; end 
       def pid; @browser.instance_variable_get :@pid; end 
+      
+      def find_by_id(id)
+        @browser.find "//div[@id='#{id}']"
+      end
     end
   end
 end
 
 include RLicker
 
+module Given
+  def given(name_and_value)
+    File.open(File.expand_path(name_and_value.keys.first),"w+") do |file|
+      file.puts name_and_value[name_and_value.keys.first]
+    end
+  end
+end
+
 shared_examples_for "a window licker" do 
+  include Given
+
   it "can open an earl" do
     lambda{licker.goto "spec/integration/samples/licker_test.html"}.must_not raise_error
   end
@@ -29,6 +43,16 @@ shared_examples_for "a window licker" do
   it "after opening an earl, it exposes it" do
     licker.goto "spec/integration/samples/licker_test.html"
     licker.url.must match %r{licker_test.html$}
+  end
+
+  it "can locate an element by id" do
+    earl = "spec/integration/samples/licker_test.html"
+    given "spec/integration/samples/licker_test.html" => <<-HTML
+      <div id="phils-bike-seat"></div>
+    HTML
+
+    licker.goto earl
+    licker.find_by_id("phils-bike-seat").must_not be_nil
   end
 
   it "you can set a base earl and then use relative ones"
